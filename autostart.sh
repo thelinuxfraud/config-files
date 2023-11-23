2ebd874a7887cd2575503d4545cd3c331246dbc1
thelinuxfraud <151329771+thelinuxfraudd@users.noreply.github.com>
2023-11-23 00:35:40 -0500

    autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+
+# Utilities
+run nm-applet &
+blueman-applet &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
+/usr/bin/dunst &
+run volumeicon &
+
+
+
+
+
